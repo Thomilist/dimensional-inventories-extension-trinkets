a961d179b9f6810507e90d02cdac6178b8203c4d
Thomas Emil Jensen <develop@thomilist.net>
2025-02-09 15:29:47 +0100

    Added static mod properties field

--- src/main/java/net/thomilist/dimensionalinventories/extension/trinkets/DimensionalInventoriesExtensionTrinkets.java
+++ src/main/java/net/thomilist/dimensionalinventories/extension/trinkets/DimensionalInventoriesExtensionTrinkets.java
@@ -2,10 +2,12 @@ package net.thomilist.dimensionalinventories.extension.trinkets;
 
 import net.thomilist.dimensionalinventories.extension.DimensionalInventoriesExtension;
 import net.thomilist.dimensionalinventories.extension.trinkets.module.TrinketsModuleGroup;
+import net.thomilist.dimensionalinventories.util.ModProperties;
 
 public class DimensionalInventoriesExtensionTrinkets
     extends DimensionalInventoriesExtension
 {
+    public static final ModProperties PROPERTIES = new ModProperties( "dimensional-inventories-extension-trinkets" );
     private static final String MOD_ID = "dimensional-inventories-extension-trinkets";
 
     public DimensionalInventoriesExtensionTrinkets()
